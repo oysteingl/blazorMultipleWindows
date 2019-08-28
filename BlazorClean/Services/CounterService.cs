68cbbdf02331e12ba39df3d4d8dc1dd948059257
Øystein Gregers Lindbjør <ogl@dips.no>
2019-08-28 09:12:34 +0200

    State updates in multiple browser windows

--- BlazorClean/Services/CounterService.cs
+++ BlazorClean/Services/CounterService.cs
@@ -1,13 +1,18 @@
+using System;
+
 namespace BlazorClean.Services
 {
     public class CounterService
     {
+        public event EventHandler<int> CounterIncremented;
         public int CurrentCount { get; set; }
 
         public int IncreaseCount()
         {
             CurrentCount += 1;
+            CounterIncremented?.Invoke(this, CurrentCount);
             return CurrentCount;
+            
         }
     }
 }
